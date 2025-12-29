a6a32fbae78a6fda730148dbb41301bfdbf53e78
Muhammad Arslan <imarslan7061@gmail.com>
2025-12-30 01:09:10 +0500

    update

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -69,5 +70,18 @@ const userSchema = new Schema(
     timestamps: true,
   },
 );
+
+// Using Pre 'save' hook to hash password before saving the user document
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// Method to compare entered password with hashed password in DB
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
 
 mongoose.model("User", userSchema);
